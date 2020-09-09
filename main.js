cc2cc1ad77ca6dcc7e85b050d570b850c6562e3b
MaxDublenko <55203253+MaxDublenko@users.noreply.github.com>
2020-09-09 13:24:22 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+// ARRAY VISUALIZER
+
+// Set up Canvas and Graphics Context
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+// Global Variables
+let myArray = [];
+
+// Giving array all values to 300
+for (i=0; i<100; i++) {
+  myArray.push(300)
+}
+
+// Main Program Loop
+requestAnimationFrame(draw);
+
+function draw() {
+    // Logic
+    let barWidth = cnv.width / myArray.length;
+
+    // Drawing
+    ctx.clearRect(0, 0, cnv.width, cnv.height);
+
+    // Draw Bar Graph
+    ctx.fillStyle = "orange";
+    ctx.strokeStyle = "grey";
+    for (let i = 0; i < myArray.length; i++) {
+        ctx.fillRect(i * barWidth, cnv.height - myArray[i], barWidth, myArray[i]);
+        ctx.strokeRect(i * barWidth, cnv.height - myArray[i], barWidth, myArray[i]);
+    }
+
+
+    // Request another Animation Frame
+    requestAnimationFrame(draw);
+}
+
+document.addEventListener('keypress', keyPressed);
+
+function keyPressed(event) {
+  let key = event.keyCode;
+  if (key == 32) {
+    for (i=0; i<myArray.length; i++) {
+      myArray[i] += Math.random() * 10 - 5
+    }
+  } else if (key == 114) {
+
+    for (i=0; i<myArray.length; i++) {
+      myArray[i] = 300;
+    }
+  }
+}
